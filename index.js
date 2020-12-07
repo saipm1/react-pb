8a0d99b13f3dc87e0ff3143b72fbbc365b077a9d
Sai Prasad <polmint@hotmail.com>
2020-12-07 23:58:21 +0530

    fifth commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+class MainClass {
+    static main() {
+        console.info("Welcome to the world of MERN!!!");
+    }
+}
+MainClass.main();
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,7 @@
+class MainClass {
+  static main(): void {
+    console.info("Welcome to the world of MERN!!!");
+  }
+}
+
+MainClass.main();
